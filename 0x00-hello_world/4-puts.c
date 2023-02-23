a9ac4e3ccc7680c6a9b8d5945f8dbe68a25e37f5
Sk-Gaitho <skgaitho1@gmail.com>
2023-02-23 14:59:51 -0800

    changes to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * Main - writes string programming is like building a multilingual puzzle to the output stream
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
